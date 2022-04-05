9ae16b24771d25fe9585c4ebbb56e32817f328bb
Dancan Chibole <dancankingstar@gmail.com>
2022-04-05 19:30:15 +0300

    Blog and category created

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    header_image = models.URLField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    blog_main_image = models.ImageField(upload_to='photos')
+    category = models.ManyToManyField(Category)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+    def __str__(self):
+        return self.title
